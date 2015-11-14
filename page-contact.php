405e6d3749171499479fe7d1b4f62557ccc4d6df
AgentEm <eeporta@gmail.com>
2015-11-13 19:48:41 -0500

    Add new image for about page, responsive update

--- page-contact.php
+++ page-contact.php
@@ -7,8 +7,12 @@
 				<section class="contact-blurb">
 					<div class="page-content-container">
 						<h2>Let's chat!</h2>
-						<p>Do you have a burning question about privacy? Do you want to know more about Privacy Horizon? Do you have a suggestion to help us improve our website or services? Please get in touch.</p>
+						<p>Do you have a burning question about privacy? Do you want to know more about Privacy Horizon? Do you have a suggestion to help us improve our website or services? Please get in touch below.</p>
 					</div>
+					<a href="#form"><div class="about-down-arrow">
+							<img src="<?php echo get_bloginfo('template_url') ?>/images/about/downarrow.svg" class="animated bounce"/>
+					</div>
+					</a>
 				</section>
         <section class="contact-form" id="form">
 					<div class="page-content-container">
